e1247e8a4ca04585925edcebf26deb64d532b5b1
Josh Zosky <imnotamember@gmail.com>
2015-07-01 23:35:16 -0500

    Updated chapter 16 file
    Now it has all of my comments and edits.

--- chapt16.py
+++ chapt16.py
@@ -4,11 +4,19 @@ script, filename = argv
 
 print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C (^C)."
+# Sidenote: this exits badly by producing an error. How could we better handle this?
+# print "If you don't want that, type 'Q'."
 print "If you do want that, hit RETURN."
 
-raw_input("?")
+response = raw_input("?")
+
+"""
+if response == 'Q':
+    quit()
+"""
 
 print "Opening the file..."
+
 target = open(filename, 'w')
 
 print "Truncating the file.  Goodbye!"
@@ -22,12 +30,19 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("%s\n%s\n%s\n" % (line1, line2, line3))
+"""
+#Alternative # 2: you could store the string to a variable like this:
+what_to_write = line1 + "\n" + line2 + "\n" + line3 + "\n"
+target.write(what_to_write)
+"""
 
 print "And finally, we close it."
+
 target.close()
+
+readit = open(filename)
+
+print readit.read()
+
+readit.close()
